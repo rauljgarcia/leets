dffab76016d95ac9fdac41a95b385cdfc4a7671f
Raul Garcia <rauljgarcia97@gmail.com>
2023-12-18 16:26:21 -0700

    added 2nd implentation

--- max_prod_diff.py
+++ max_prod_diff.py
@@ -19,11 +19,35 @@ Output: 34
 '''
 def maxProductDifference(self, nums):
     '''
-    Implementation 1 using builtin sort function
+    Implementation 1 using builtin sort() function
     Runtime: 165 ms
     Memory: 14.39 mb
     '''
-
     nums.sort()
     return nums[len(nums)-1] * nums[len(nums)-2] - nums[0] * nums[1]
 
+def maxProductDifference2(self, nums):
+    '''
+    Implementation 2 using a manual sort of the list
+    Runtime: 120ms
+    Memory: 14.29 mb
+    '''
+    #initialize variables
+    largest = 0 
+    second_largest = 0
+    smallest = max(nums)
+    second_smallest = max(nums)
+    
+    #sort list
+    for i in nums:
+        if i < smallest:
+            second_smallest = smallest
+            smallest = i
+        elif i < second_smallest:
+            second_smallest = i
+        if i > largest:
+            second_largest = largest
+            largest = i
+        elif i > second_largest:
+            second_largest = i
+    return largest*second_largest-smallest*second_smallest
